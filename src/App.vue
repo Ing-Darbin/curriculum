07d6fe9253298e09091313d8394b56fdc65c338f
Ing-Darbin <darbin.salgado121@est.unanleon.edu.ni>
2024-04-20 00:16:26 -0600

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,196 @@
+<template>
+  <div>
+    <img src="@/assets/logo.jpg" alt="Mi Imagen" />
+   
+    <div class="tema"><h1><strong>Curriculum vitae</strong></h1></div>
+    <div class="titulo1"><h2>DATOS PERSONALES
+    </h2></div>
+    <div class="datos_personales">
+      <p><strong>NOMBRES Y APELLIDOS:</strong>  Darbin Santiago Salgado Pereira.
+        <br>
+<strong>LUGAR Y FECHA DE NACIMIENTO:</strong>  Comarca las Mercedes, Quezalguaque-Leon. 25 Julio del año 1981.
+<br>
+<strong> CEDULA DE IDENTIDAD:</strong>  286-250781-0001K.
+<br>
+<strong>SEXO:</strong>  Masculino.
+<br>
+<strong> ESTADO CIVIL:</strong> Casado.
+<br>
+<strong> DIRECCION DOMICILIAR:</strong> Comarca Las Mercedes 100mts. Al sur, Kilometro 106 Carretera Leon - Chinandega.
+<br>
+<strong>TELEFONO:</strong> 5850-2009 (Claro) 7743-5243 (Tigo).
+<br>
+<strong>EMAIL:</strong> pereiradarwing81@gmail.com , pereiradarwing81@outlook.com , darbin.salgado121@est.unanleon.edu.ni 
+</p>
+    </div>
+        <div class="titulo2"><h2>EXPERIENCIA LABORAL</h2></div>
+    
+        <p><strong>COOPERATIVA LAS MERCEDES R.L:</strong> como Operador de máquinas moto guadañas 8 años.
+          <br>
+          como contador de la cooperativa desde el 2013 hasta la actualidad contratado como servicios profesionales.
+          <br>
+        <strong>CAPS LAS MERCEDES FE Y ESPERANZA:</strong> como contador 2 años, como cajero en la actualidad.
+          <br>
+        <strong>COMPUCELL (TALLER DE MANTENIMIENTO ,REPARACION DE CELUALRES Y COMPUTAODRAS)</strong> Negocio propio 6 años como técnico.
+          <br>
+        <strong>PROCESADORA DE MANI JEHOVA YAHWEH:</strong> como contador y jefe de area de Procesamiento, una zafra laboré</p>
+  </div>
+  <div class="formacion">
+  <div class="titulo3"><h2><strong>FORMACION</strong></h2></div>
+    <p><strong>ESTUDIOS UNIVERSITARIOS:</strong> 4to. año de la carrera de Ingenieria en Tecnologias con énfasis en desarrollo web y marketing digital. en la universidad nacional autónoma de nicaragua "UNAN - LEON".
+      <br>
+    <strong>BACHILLERATO:</strong>  Escuela pública Marcelina Peralta de Ramirez. bachiller en ciencias y letras.
+      <br>
+    <strong>EDUCACION PRIMARIA:</strong> Escuela pública Marcelina Peralta de Ramirez.</p>
+  </div>
+  <div class="titulo4"><h2><strong>CAPACITACIONES EN LOS ULTIMOS  3 AÑOS</strong></h2></div>
+  <p>En desarrollo web con HTML, CSS y JavaScript.
+
+    En creación de contenidos audio visuales.
+    
+    En informática. </p>
+    <div class="titulo5"><h2><strong>INVESTIGACIONES REALIZADAS (QUE HA REALIZADO O QUE LE GUSTARIA REALIZAR)</strong></h2></div>
+    <p>Me Gustaría Saber acerca o a profundidades lo que es hacking ético y sistema operativo Kali Linux. </p>
+    <div class="titulo6"><h2><strong>REDES SOCIALES</strong></h2>
+
+      <a class ="a1" href="https://www.facebook.com/profile.php?id=100073477808671" target="_blank">Facebook</a>
+      <br>
+      <br>
+      <a class ="a2" href="https://www.twitter.com/DarwingPer45305" target="_blank">Twitter</a>
+    
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body{
+ 
+  background-color: rgb(4, 92, 91);
+
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  color: rgb(234, 234, 224);
+  font-size: 20px;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+
+}
+
+img {
+  width: 200px;
+  height: 250px;
+  display: block;
+  margin: 0 auto;
+  border-radius: 20%;
+  border: 6px solid rgb(6, 27, 1);
+}
+.tema{
+  color: rgb(237, 188, 26);
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+  
+}
+p{
+  text-align: left;
+
+}
+.titulo1 {
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  color: rgb(210, 14, 14);
+  margin-top: 2px;
+  padding:10;
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border: 4px solid;
+  border-radius: 20px;
+  border-color: black;
+  background-color:rgb(145, 147, 21);
+}
+.titulo2 {
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  color: rgb(210, 14, 14);
+  margin-top: 2px;
+  padding:10;
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border: 4px solid;
+  border-radius: 20px;
+  border-color: black;
+  background-color:rgb(145, 147, 21);
+}
+.titulo3 {
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  color: rgb(210, 14, 14);
+  margin-top: 2px;
+  padding:10;
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border: 4px solid;
+  border-radius: 20px;
+  border-color: black;
+  background-color:rgb(145, 147, 21);
+}
+.titulo4 {
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  color: rgb(210, 14, 14);
+  margin-top: 2px;
+  padding:10;
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border: 4px solid;
+  border-radius: 20px;
+  border-color: black;
+  background-color:rgb(145, 147, 21);
+}
+.titulo5 {
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+  color: rgb(210, 14, 14);
+  margin-top: 2px;
+  padding:10;
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border: 4px solid;
+  border-radius: 20px;
+  border-color: black;
+  background-color:rgb(145, 147, 21);
+}
+.a1{
+  text-decoration: none;
+  color: rgb(104, 11, 244);
+  font-weight: bold;
+  font-size: 30px;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border-radius: 20px;
+  
+}
+.a2{
+  text-decoration: none;
+  color: rgb(21, 20, 22);
+  font-weight: bold;
+  font-size: 30px;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  border-radius: 20px;
+  
+  
+}
+
+
+
+</style>
+
